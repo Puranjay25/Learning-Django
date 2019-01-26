2836f161a6ba862fa01d11b036779545fe4fbfb6
puranjay25 <puranjay.makhija@gmail.com>
2019-01-26 15:52:34 +0530

    Upgraded Django module

--- model/main/models.py
+++ model/main/models.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
-class first(models.Model):
-	name=models.TextField()
-	
-	def __str__(self):
-	    return self.name
