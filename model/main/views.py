22a20f40e4dc2ad81f78ab10b7f236a5c7fee5d2
puranjay25 <puranjay.makhija@gmail.com>
2019-01-28 22:10:27 +0530

    Added forms

--- model/main/views.py
+++ model/main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def home(request):
 	return render(request=request,template_name="home.html")
+
+def register(request):
+	form=UserCreationForm
+	return render(request=request,template_name="register.html",context={"form":form})
