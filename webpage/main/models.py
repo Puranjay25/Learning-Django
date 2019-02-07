92b53699c7b760678bcce6fbecf4596da601d48c
puranjay25 <puranjay.makhija@gmail.com>
2019-02-07 20:08:53 +0530

    continued

--- webpage/main/models.py
+++ webpage/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class details(models.Model):
+	name=models.TextField()
+	city=models.TextField()
+
+	def __str__(self):
+		return self.name
--- webpage/main/views.py
+++ webpage/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
@@ -6,6 +6,12 @@ def home(request):
 	return render(request=request,template_name="home.html")
 
 def signup(request):
+	if request.method=="POST":
+		form=UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect("main:home")
+
 	form=UserCreationForm
 	return render(request,"signup.html",context={"form":form})
 
